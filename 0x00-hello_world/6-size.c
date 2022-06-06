7d871b4ec2b8fc91684e226d2246f7ddd0291f79
Anthony Odion <okhuelegbeanthony3@gmail.com>
2022-06-06 15:51:04 -0700

    Betty please

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
-return (0); 
-}
+return (0); }
 
