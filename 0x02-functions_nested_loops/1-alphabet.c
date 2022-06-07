104feab1b48a9baf0ccab735eda6f4615f429ed2
Anthony Odion <okhuelegbeanthony3@gmail.com>
2022-06-07 05:01:25 -0700

    God please

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lower case
+ *
+ * Return: void
+ * print_alphabet: lower cases only ' found in main.h
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
