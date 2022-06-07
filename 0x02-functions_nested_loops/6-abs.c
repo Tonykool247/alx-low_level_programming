86d3c1ab961e2410d840a196e426b05fe06e22f8
Anthony Odion <okhuelegbeanthony3@gmail.com>
2022-06-07 07:50:00 -0700

    Task done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - print absolutr value
+ * @n: integer
+ * Return: integer
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
